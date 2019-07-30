c5b27ebb721418354db9519322162f2e9ce8d460
kiyoshimori <Darkf0x13566@gmail.com>
2019-07-30 22:33:30 +0300

    connect postgres and sequelize

---
+++ src/lib/db/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+	host: 'localhost',
+	dialect: 'postgres',
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	},
+});
+
+export default sequelize;
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,16 @@ import express, { Express } from 'express';
 
 import startServer from './graphql';
 import webpackCompiler from './webpackCompiler';
+import db from '../lib/db';
+
+db
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err: string) => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 const server: Express = express();
 
