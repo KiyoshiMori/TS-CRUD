0ae9de6dd7f98326e27e2d2d1734a8deda99ca4b
kiyoshimori <Darkf0x13566@gmail.com>
2019-07-31 12:10:13 +0300

    add prerttier and prettify some code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'prettier'],
     extends: ['plugin:@typescript-eslint/recommended'],
+    rules: {
+        'prettier/prettier': 'error',
+    },
 };
--- src/shared/mainPage.tsx
+++ src/shared/mainPage.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import { Query } from 'react-apollo';
 
 import { getRecipe } from 'lib/graphql/queries/recipe';
